dc0e88c46cb60e3ada7c7b585dbc13cb48814be3
MISTAKELY <thereisamailforme@gmail.com>
2026-02-23 11:54:38 +0545

    initilized:user dashboard

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from "../controller/product.controllers.js";
+import { createProduct, deleteProduct, getProduct, getProducts, searchProducts, updateProduct } from "../controller/product.controllers.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const productsRoute = express.Router();
 
 productsRoute.post("/createProduct",verifyToken, createProduct);
 productsRoute.post("/getProduct", getProducts);
-productsRoute.post("/getProducts", getProduct);
+productsRoute.get("/getProducts", getProduct);
 productsRoute.post("/updateProduct", updateProduct);
 productsRoute.post("/deleteProduct", deleteProduct);
+productsRoute.get("/searchProducts", searchProducts);
 
 export default productsRoute;
