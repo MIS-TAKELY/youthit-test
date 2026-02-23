30d5902c526b68e0744070b3049b46bff5db6731
MISTAKELY <thereisamailforme@gmail.com>
2026-02-23 10:53:56 +0545

    added:cors

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import authRouter from "./src/routes/auth.routes.js";
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", authRouter);
